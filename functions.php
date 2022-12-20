e1488a298bcb76fe2f412eece1d2360a96ca0589
axyn45 <axyn45@outlook.com>
2022-12-20 08:51:38 +0000

    Site statistics header

--- functions.php
+++ functions.php
@@ -2026,3 +2026,40 @@ function should_show_title():bool{
     || !get_post_thumbnail_id($id) 
     && $use_as_thumb != 'true' && !get_post_meta($id, 'video_cover', true);
 }
+
+//WordPress获取站点总浏览量
+function total_views() /*注意这个函数名，调用的就是用它了*/
+{
+global $wpdb;
+$count=0;
+$views= $wpdb->get_results("SELECT * FROM $wpdb->postmeta WHERE meta_key='views'");
+foreach($views as $key=>$value)
+{
+$meta_value=$value->meta_value;
+if($meta_value!=' ')
+{
+$count+=(int)$meta_value;}
+}
+return $count;}
+ 
+ 
+//总评论
+ 
+function comment_users($postid=0,$which=0) {
+    $comments = get_comments('status=approve&type=comment&post_id='.$postid); //获取文章的所有评论
+    if ($comments) {
+        $i=0; $j=0; $commentusers=array();
+        foreach ($comments as $comment) {
+            ++$i;
+            if ($i==1) { $commentusers[] = $comment->comment_author_email; ++$j; }
+            if ( !in_array($comment->comment_author_email, $commentusers) ) {
+                $commentusers[] = $comment->comment_author_email;
+                ++$j;
+            }
+        }
+        $output = array($j,$i);
+        $which = ($which == 0) ? 0 : 1;
+        return $output[$which]; //返回评论人数
+    }
+    return 0; //没有评论返回 0
+}
--- layouts/feature_v2.php
+++ layouts/feature_v2.php
@@ -8,6 +8,15 @@
     $exhibition = iro_opt('exhibition');
 
 ?>
+
+<div class="top-text">
+    <i class="fa fa-folder-open" aria-hidden="true"></i><font style="color:#F00; font-weight:bold;">文章</font>:<?php $count_posts = wp_count_posts(); echo $published_posts = $count_posts->publish; ?>篇&nbsp;&nbsp;
+    <i class="fa fa-eye" aria-hidden="true"></i><font style="color:#F00; font-weight:bold;">阅读</font>:<?php echo total_views(); ?>次&nbsp;&nbsp;
+    <i class="fa fa-comments" aria-hidden="true"></i><font style="color:#F00; font-weight:bold;">评论</font>:<?php echo comment_users($postid, 1); ?>次&nbsp;&nbsp;
+    <i class="fa fa-check" aria-hidden="true"></i><font style="color:#F00; font-weight:bold;">最后更新</font>:<?php the_modified_time('Y年n月j日'); ?>&nbsp;&nbsp;
+    <i class="fa fa-power-off" aria-hidden="true"></i><font style="color:#F00; font-weight:bold;">运行</font>:<?php echo floor((time()-strtotime("2022-10-20"))/86400);?>天
+</div>
+
 <div class="top-feature-row">
     <h1 class="fes-title"> <i class="<?php echo iro_opt('exhibition_area_icon', 'fa fa-laptop'); ?>" aria-hidden="true"></i> <br> <?php echo iro_opt('exhibition_area_title', '展示'); ?> </h1>
     <div class="top-feature-v2">
